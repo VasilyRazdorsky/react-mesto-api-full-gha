15e0ad769d912b18c1f1f8370f236ddafe89ff6c
Vasiliy Razdorskiy <vrazdorsky@yandex.ru>
2023-04-06 22:28:09 +0300

    delete cors

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
-const { handleCors } = require('./middlewares/cors');
+//const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { errors, celebrate, Joi } = require('celebrate');
 const bodyParser = require('body-parser');
@@ -19,7 +18,6 @@ const { PORT = 3001 } = process.env;
 const app = express();
 
 //app.use(cors());
-app.use(handleCors);
 
 app.use(bodyParser.json());
 
