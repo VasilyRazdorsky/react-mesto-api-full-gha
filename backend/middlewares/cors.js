1cf9459d5f3117f6391fc06895324d37d3ce5d23
Vasiliy Razdorskiy <vrazdorsky@yandex.ru>
2023-04-06 19:47:59 +0300

    Create handleCors func

---
+++ backend/middlewares/cors.js
@@ -0,0 +1,19 @@
+const handleCors = (req, res, next) => {
+  const { method } = req;
+
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control_Allow-Credentials', true);
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
+}
+
+module.exports = handleCors;
